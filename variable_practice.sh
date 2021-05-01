7e03507351c1c406ce260e9f07d7157d90ab1498
Isaac_K <66635295+Isaac-Ken@users.noreply.github.com>
2021-05-01 10:14:00 -0500

    Create variable_practice.sh

---
+++ variable_practice.sh
@@ -0,0 +1,14 @@
+!/bin/bash
+
+#name=value (no spaces)
+
+#Email generator Enter emplyees name to make mail
+echo "Enter Employee Name:"
+read employee
+echo "Dear ${employee}"
+echo "The Company would like to acknowledges you for all the hardwork youve done this quarter."
+echo "Keep it up!"
+
+echo "Thanks,"
+echo "Soda CEO"
+exit=0
