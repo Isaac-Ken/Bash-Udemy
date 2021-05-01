3affb84d8435a1e0db05d4783a1669c0bb44fac6
Isaac_K <66635295+Isaac-Ken@users.noreply.github.com>
2021-05-01 10:51:35 -0500

    Update backup_script.sh

--- backup_script.sh
+++ backup_script.sh
@@ -1,7 +1,12 @@
 ! /bin/bash
 
 
-#Desc:This script backsup all the files in your home directory and save them all in a .tar archive
+#Desc:This script backsup all the files in your home directory and saves them in a .tar archive as my_backup_dd-mm-yyyy_HH-MM-SS to the ~/bash_course folder
+#c tells tar to create a new archive,
+#v sets the screen output to verbose
+#f points to the filename to give the archive.
+#The * is what tells tar to include all files and local directories recursively.
+#2>/dev/null will filter out the errors so that they will not be output to your console.
 #Usage:/backup_script
 
 tar -cvf ~/bash_course/my_backup_"$(date +%d-%m-%Y_%H-%M-%S)".tar ~/* 2>/dev/null
