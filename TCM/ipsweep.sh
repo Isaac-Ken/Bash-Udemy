095ac25a874a16fa8964c9c410708e5956448a61
Isaac_K <66635295+Isaac-Ken@users.noreply.github.com>
2022-04-21 19:37:28 -0500

    Update ipsweep.sh

--- TCM/ipsweep.sh
+++ TCM/ipsweep.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
+## This tool scans for active IP Addresses
 if [ "$1" == "" ]
-then
-echo "You forgot an IP address"
-echo "Syntax: ./ipsweep.sh 192.168.0"
-
+	then
+	echo "You forgot an IP address!"
+	echo "Syntax: ./ipsweep.sh 192.168.0"
 else
-for ip in `seq 1 254`; do
-ping -c l $1.$ip |grep "64 Bytes" | cut -d " " -f 4| tr -d ":" &
-done
+	for ip in `seq 1 254`; do
+	ping -c 1 $1.$ip | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" &
+  done
 fi
