b9758933510a04ba5f9b00d618bd73854f3e9e08
Isaac_K <66635295+Isaac-Ken@users.noreply.github.com>
2022-04-21 19:29:46 -0500

    Update ipsweep.sh

--- TCM/ipsweep.sh
+++ TCM/ipsweep.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+if [ "$1"=="" ]
+then
+echo "You forgot an IP address"
+echo "Syntax: .ipsweep.sh 192.168.0"
+
+else
 for ip in `seq 1 254`; do
 ping -c l $1.$ip |grep "64 Bytes" | cut -d " " -f 4| tr -d ":" &
 done
-
-./ipsweep.sh 192.168.0
+fi
